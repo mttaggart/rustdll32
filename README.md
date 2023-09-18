8d8644f52b80f1305fb52136d0d58417e17b8537
Michael Taggart <mtaggart@taggart-tech.com>
2023-09-17 22:09:34 -0700

    Start it up

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# rustdll32
+# Rustdll32: A Template for Rust DLLs
+
+## Purpose
+
+I frequently build Rust-based DLLs for security work, as demoed in [this YouTube Video](https://www.youtube.com/watch?v=K6dAquqj5E4). I figured it would save time to create a template for doing so.
+
+
+## Usage
+
+1. Clone this repo
+2. Write your code in `lib.rs::do_stuff()`. 
+3. `cargo build --target x86_64-pc-windows-gnu`
+4. Enjoy your DLL!
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use windows::{
+    core::PCSTR,
+    Win32::{
+        Foundation::{BOOL, HANDLE, HWND},
+        UI::WindowsAndMessaging::{MessageBoxA, MESSAGEBOX_STYLE},
+    },
+};
+
+#[no_mangle]
+extern "C" fn do_stuff() {
+    unsafe {
+        MessageBoxA(
+            HWND(0),
+            PCSTR("Doing stuff!\x00".as_ptr()),
+            PCSTR("Hello!\x00".as_ptr()),
+            MESSAGEBOX_STYLE(0),
+        );
+    }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+extern "system" fn DLLMain(dll_module: HANDLE, call_reason: u32, lpv_reserved: u32) -> BOOL {
+    match call_reason {
+        _ => {
+            do_stuff();
+            BOOL(0)
+        }
+    }
+}
